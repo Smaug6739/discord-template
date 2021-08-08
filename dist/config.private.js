bf8654f9bd42237399684ae4babb41a238acfbc9
Raphael <veauville.raphael@gmail.com>
2021-08-08 09:47:52 +0200

    Remove config.ts

---
+++ dist/config.private.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    tokens: {
+        YOUTUBE: '',
+        discord: 'ODA5NzAxMzA2NDQ0MDIxNzky.YCY7Gg.aAtX9UA9btiJqLojLW_mQxWtSwo'
+    },
+    mongoose: {
+        connection: 'mongodb://localhost:27017/spirituslocal'
+    },
+    logs: {
+        error: {
+            id: '730122264908922940',
+            token: 'xPZMNjfuIyyarIi1Rvca5phog3LA0khTydHQAuBPBqkKlRAB50ENfFZhMcmqNR7_8v2_',
+            name: 'Logs errors',
+            avatar: 'https://cdn.discordapp.com/attachments/736127175471792159/736127787911479356/1589812181984.png',
+        },
+        info: {
+            id: '725313155236102164',
+            token: 'iYdWlcVjDnW6ty4-v6PXNFZ6EL2KPe7g0GPRWJbXgyZpAwT5PB2OJKLaTVNcB3MlFDXP',
+            name: 'Logs',
+            avatar: 'https://cdn.discordapp.com/attachments/736127175471792159/736127357852844092/1589811752460.png',
+        },
+    },
+};
