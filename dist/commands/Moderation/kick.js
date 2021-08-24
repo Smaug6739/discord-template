c70e094e946cc88c19b2f209c8f446513aafbb02
Raphael <veauville.raphael@gmail.com>
2021-08-24 09:49:18 +0200

    Fix colors

--- dist/commands/Moderation/kick.js
+++ dist/commands/Moderation/kick.js
@@ -17,43 +17,45 @@ const CommandClass_1 = __importDefault(require("../CommandClass"));
 class default_1 extends CommandClass_1.default {
     constructor(bot) {
         super(bot, {
-            name: 'kick',
+            name: "kick",
             aliases: [],
             options: [
                 {
-                    name: 'user',
-                    description: 'User to be banned',
-                    type: 'STRING',
-                    required: true
+                    name: "user",
+                    description: "User to be banned",
+                    type: "STRING",
+                    required: true,
                 },
                 {
-                    name: 'reason',
-                    description: 'The reason of ban',
-                    type: 'STRING',
-                    required: false
+                    name: "reason",
+                    description: "The reason of ban",
+                    type: "STRING",
+                    required: false,
                 },
             ],
-            description: 'Kick user from the server',
-            category: 'Moderation',
+            description: "Kick user from the server",
+            category: "Moderation",
             cooldown: 5,
-            userPermissions: ['MODERATOR'],
-            botPermissions: ['KICK_MEMBERS'],
+            userPermissions: ["MODERATOR"],
+            botPermissions: ["KICK_MEMBERS"],
             subCommands: [],
         });
     }
     execute(interaction, args, settings) {
         return __awaiter(this, void 0, void 0, function* () {
-            const argUser = args.get('user').value;
-            const reason = args.get('reason').value || 'No reason was given';
+            const argUser = args.get("user").value;
+            const reason = args.get("reason").value || "No reason was given";
             const interactionMember = yield this.bot.util.resolveMember(interaction.guild, interaction.user.id);
             const user = yield this.bot.util.resolveMember(interaction.guild, argUser);
             if (!user)
                 return interaction.replyErrorMessage(`User not found.`);
-            if (interactionMember.roles.highest.comparePositionTo(user.roles.highest) <= 0 && interaction.guild.ownerId !== interaction.user.id)
+            if (interactionMember.roles.highest.comparePositionTo(user.roles.highest) <=
+                0 &&
+                interaction.guild.ownerId !== interaction.user.id)
                 return interaction.replyErrorMessage(`You don't have the permission for this.`);
             const embed = new discord_js_1.MessageEmbed()
                 .setAuthor(`${user.user.username} (${user.id})`)
-                .setColor(`${this.bot.colors.orange}`)
+                .setColor(this.bot.colors.orange)
                 .setDescription(`**Action**: kick\n**Reason**: ${reason}`)
                 .setThumbnail(user.user.displayAvatarURL())
                 .setTimestamp()
@@ -67,7 +69,10 @@ class default_1 extends CommandClass_1.default {
                         interaction.reply({ embeds: [embed] });
                         if (settings.modLogs) {
                             const channel = this.bot.util.resolveChannel(interaction.guild, settings.modLogs);
-                            if (channel && channel.permissionsFor(interaction.guild.me).has('SEND_MESSAGES'))
+                            if (channel &&
+                                channel
+                                    .permissionsFor(interaction.guild.me)
+                                    .has("SEND_MESSAGES"))
                                 channel.send(embed);
                         }
                     });
--- dist/commands/Moderation/mute.js
+++ dist/commands/Moderation/mute.js
@@ -18,45 +18,45 @@ const CommandClass_1 = __importDefault(require("../CommandClass"));
 class default_1 extends CommandClass_1.default {
     constructor(bot) {
         super(bot, {
-            name: 'mute',
+            name: "mute",
             aliases: [],
             options: [
                 {
-                    name: 'user',
-                    description: 'User to mute.',
-                    type: 'STRING',
-                    required: true
+                    name: "user",
+                    description: "User to mute.",
+                    type: "STRING",
+                    required: true,
                 },
                 {
-                    name: 'time',
-                    description: 'Time of mute (default : 1h).',
-                    type: 'STRING',
-                    required: false
+                    name: "time",
+                    description: "Time of mute (default : 1h).",
+                    type: "STRING",
+                    required: false,
                 },
             ],
-            description: 'Mute user from the server.',
-            category: 'Moderation',
+            description: "Mute user from the server.",
+            category: "Moderation",
             cooldown: 5,
-            userPermissions: ['MODERATOR'],
-            botPermissions: ['MANAGE_CHANNELS'],
+            userPermissions: ["MODERATOR"],
+            botPermissions: ["MANAGE_CHANNELS"],
             subCommands: [],
         });
     }
     execute(interaction, args, settings) {
         var _a;
         return __awaiter(this, void 0, void 0, function* () {
-            const argUser = args.get('user').value;
-            const argTime = (_a = args.get('time')) === null || _a === void 0 ? void 0 : _a.value;
+            const argUser = args.get("user").value;
+            const argTime = (_a = args.get("time")) === null || _a === void 0 ? void 0 : _a.value;
             const user = yield this.bot.util.resolveMember(interaction.guild, argUser);
-            let muteRole = interaction.guild.roles.cache.find(r => r.name === 'Muted');
-            const muteTime = argTime || '60s';
+            let muteRole = interaction.guild.roles.cache.find((r) => r.name === "Muted");
+            const muteTime = argTime || "60s";
             if (!user)
                 return interaction.replyErrorMessage(`User not found.`);
             if (!muteRole) {
                 muteRole = yield interaction.guild.roles.create({
-                    name: 'Muted',
-                    color: '#2f3136',
-                    permissions: []
+                    name: "Muted",
+                    color: "#2f3136",
+                    permissions: [],
                 });
                 // interaction.guild!.channels.cache.forEach(async (channel) => {
                 // 	await channel.updateOverwrite(muteRole!, {
@@ -66,7 +66,6 @@ class default_1 extends CommandClass_1.default {
                 // 	});
                 // });
             }
-            ;
             yield user.roles.add(muteRole.id);
             interaction.replySuccessMessage(`<@${user.id}> is muted for ${ms_1.default(ms_1.default(muteTime))}.`);
             setTimeout(() => {
@@ -74,14 +73,14 @@ class default_1 extends CommandClass_1.default {
             }, ms_1.default(muteTime));
             const embed = new discord_js_1.MessageEmbed()
                 .setAuthor(`${user.user.username} (${user.id})`, user.user.displayAvatarURL())
-                .setColor(`${this.bot.colors.ORANGE}`)
+                .setColor(this.bot.colors.ORANGE)
                 .setDescription(`**Action**: mute\n**Time**: ${ms_1.default(ms_1.default(muteTime))}`)
                 .setTimestamp()
                 .setFooter(interaction.user.username, interaction.user.displayAvatarURL());
             if (settings.modLogs) {
                 const channel = this.bot.util.resolveChannel(interaction.guild, settings.modLogs);
                 if (channel) {
-                    if (channel.permissionsFor(interaction.guild.me).has('SEND_MESSAGES')) {
+                    if (channel.permissionsFor(interaction.guild.me).has("SEND_MESSAGES")) {
                         channel.send({ embeds: [embed] });
                     }
                 }
